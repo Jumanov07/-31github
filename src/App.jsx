6b9c993a863e3f457441b272088de0c761531d94
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-11-10 12:55:33 +0600

    solved the first problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return <h1>#31github</h1>;
 };
 
 export default App;
---
+++ src/tasks/first.js
@@ -0,0 +1,20 @@
+const createCounter = (init) => {
+  let counter = init;
+
+  return {
+    increment: () => {
+      counter += 1;
+      return counter;
+    },
+    decrement: () => {
+      if (counter !== 0) {
+        counter -= 1;
+      }
+      return counter;
+    },
+    reset: () => {
+      counter = init;
+      return counter;
+    },
+  };
+};
