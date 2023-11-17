803d0cca0ba31e03926d7647198b3829e8f03704
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-11-17 15:11:09 +0600

    solved the sixth problem

---
+++ tasks/sixth.js
@@ -0,0 +1,10 @@
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const sortedS = s.split("").sort().join("");
+  const sortedT = t.split("").sort().join("");
+
+  return sortedS === sortedT;
+};
