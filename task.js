559f1df171e0c4de6a95627984df63faa31cd6b7
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-12-01 14:57:36 +0600

    solved the twenteeth problem

---
+++ task.js
@@ -0,0 +1,13 @@
+const numJewelsInStones = (jewels, stones) => {
+  let count = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i])) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+numJewelsInStones("aA", "aAAbbbb");
