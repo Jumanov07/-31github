0a9340060b9ede5458a23a88b015c43a8946e7a1
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-12-02 17:58:44 +0600

    solved the twenty-first problem

--- task.js
+++ task.js
@@ -1,13 +1,30 @@
-const numJewelsInStones = (jewels, stones) => {
+//21
+const finalValueAfterOperations = (operations) => {
   let count = 0;
 
-  for (let i = 0; i < stones.length; i++) {
-    if (jewels.includes(stones[i])) {
-      count++;
+  for (let i = 0; i < operations.length; i++) {
+    switch (operations[i]) {
+      case "--X":
+        count--;
+        break;
+
+      case "X--":
+        count--;
+        break;
+
+      case "++X":
+        count++;
+        break;
+
+      case "X++":
+        count++;
+        break;
+
+      default:
+        count = 0;
+        break;
     }
   }
 
   return count;
 };
-
-numJewelsInStones("aA", "aAAbbbb");
