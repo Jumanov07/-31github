642c64e65a00ee765ae5db502b6467f7f94c3bef
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-12-03 04:35:12 +0600

    solved the twenty-second problem

--- task.js
+++ task.js
@@ -1,30 +1,6 @@
-//21
-const finalValueAfterOperations = (operations) => {
-  let count = 0;
-
-  for (let i = 0; i < operations.length; i++) {
-    switch (operations[i]) {
-      case "--X":
-        count--;
-        break;
-
-      case "X--":
-        count--;
-        break;
-
-      case "++X":
-        count++;
-        break;
-
-      case "X++":
-        count++;
-        break;
-
-      default:
-        count = 0;
-        break;
-    }
-  }
-
-  return count;
+//22
+const interpret = (command) => {
+  return command.split("()").join("o").split("(al)").join("al");
 };
+
+interpret("G()(al)");
